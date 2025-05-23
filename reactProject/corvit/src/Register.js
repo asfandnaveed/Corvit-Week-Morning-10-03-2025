fbea063b6bd489544397165c26a0b1b75ff1633b
Hina Liaqat <m.asfandnaveed@gmail.com>
2025-05-23 10:59:47 +0500

    changes

--- reactProject/corvit/src/Register.js
+++ reactProject/corvit/src/Register.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Register(){
+
+    const [name , setName] = useState("Hassan");
+    const [email , setEmail] = useState("Hassan@gmail.com");
+    const [password , setPassword] = useState("123456");
+    const [address , setAddress] = useState("Lahore , Paksitan");
+    const [phone , setPhone] = useState("030000000");
+
+    const data = {
+        name : name,
+        email : email,
+        pass: password,
+        address: address,
+        ph_no:phone
+    };
+
+    const result = fetch('http://localhost:3001/user/register',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
 
 
     return(
